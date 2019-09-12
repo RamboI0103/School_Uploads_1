5b065196b176760f5c41aa3b0cff43821b6aa464
RamboI0103 <55092847+RamboI0103@users.noreply.github.com>
2019-09-12 08:16:47 -0500

    Add files via upload

---
+++ Ascii_Art.java
@@ -0,0 +1,24 @@
+package output;
+
+public class Ascii_Art 
+{
+	public static void main(String[] args)
+	{
+
+	System.out.println("ΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩ");
+	System.out.println("ΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩ\t\t\tΩΩ");
+	System.out.println("ΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩ");
+	System.out.println("ΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩ");
+	}
+}
